89e54902cf8327927f33724b0a7200a08a11f13d
Michał <bukaniex@gmail.com>
2018-05-01 15:23:08 +0900

    back to old look to implement better look

--- dm_system/views.py
+++ dm_system/views.py
@@ -3,7 +3,7 @@ from .models import SingleRow, SingleImage, SingleImageId
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'dm_system/index2.html')
 
 def allrows(request):
     rows = SingleRow.objects.order_by('id')
